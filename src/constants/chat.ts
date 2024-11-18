c9c736db9e775cda977cc665cce46217a162c003
AquaMCVN <120324946+AquaMCVN@users.noreply.github.com>
2024-11-18 21:41:07 +0700

    Update chat.ts

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -29,7 +29,7 @@ export const modelOptions: ModelOptions[] = [
   'gpt-4-turbo',
   'gpt-4-turbo-2024-04-09',
   'gpt-4o',
-  'gpt-4o-2024-05-13',
+  'chatgpt-4o-latest',
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
   // 'gpt-4-32k-0314',
@@ -56,7 +56,7 @@ export const modelMaxToken = {
   'gpt-4-turbo': 128000,
   'gpt-4-turbo-2024-04-09': 128000,
   'gpt-4o': 128000,
-  'gpt-4o-2024-05-13': 128000,
+  'chatgpt-4o-latest': 108000,
 };
 
 export const modelCost = {
@@ -132,7 +132,7 @@ export const modelCost = {
     prompt: { price: 0.005, unit: 1000 },
     completion: { price: 0.015, unit: 1000 },
   },
-  'gpt-4o-2024-05-13': {
+  'chatgpt-4o-latest': {
     prompt: { price: 0.005, unit: 1000 },
     completion: { price: 0.015, unit: 1000 },
   },
