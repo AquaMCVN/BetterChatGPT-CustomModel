aa3e90605942a9c9d62504e099fdea4c35a137f4
AquaMCVN <120324946+AquaMCVN@users.noreply.github.com>
2024-12-08 01:07:16 +0700

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -19,12 +19,14 @@ export const getChatCompletion = async (
     headers['api-key'] = apiKey;
 
     const modelmapping: Partial<Record<ModelOptions, string>> = {
+      'gpt-3.5-turbo': 'gpt-35-turbo',
+      'gpt-3.5-turbo-16k': 'gpt-35-turbo-16k',
+      'gpt-3.5-turbo-1106': 'gpt-35-turbo-1106',
+      'gpt-3.5-turbo-0125': 'gpt-35-turbo-0125',
       'gpt-4o': 'gpt-4o',
       'gpt-4o-mini': 'gpt-4o-mini',
       'gpt-4-turbo': 'gpt-4-turbo',
       'gpt-4': 'gpt-4',
-      'gpt-3.5-turbo-16k': 'gpt-35-turbo-16k',
-      'gpt-3.5-turbo-1106': 'gpt-35-turbo-1106',
       'o1-mini': 'o1-mini',
       'o1-preview': 'o1-preview',
       'claude-3-5-sonnet-20241022': 'claude-3-5-sonnet',
@@ -35,12 +37,11 @@ export const getChatCompletion = async (
 
     const model = modelmapping[config.model] || config.model;
 
+    // set api version to 2023-07-01-preview for gpt-4 and gpt-4-32k, otherwise use 2023-03-15-preview
     const apiVersion =
-      ['gpt-4', 'gpt-4o', 'gpt-4o-mini', 'gpt-4-turbo'].includes(model)
+      model === 'gpt-4' || model === 'gpt-4-32k'
         ? '2023-07-01-preview'
-        : ['gpt-3.5-turbo-16k', 'gpt-3.5-turbo-1106'].includes(model)
-        ? '2023-03-15-preview'
-        : '2023-01-01';
+        : '2023-03-15-preview';
 
     const path = `openai/deployments/${model}/chat/completions?api-version=${apiVersion}`;
 
@@ -84,12 +85,13 @@ export const getChatCompletionStream = async (
     headers['api-key'] = apiKey;
 
     const modelmapping: Partial<Record<ModelOptions, string>> = {
+      'gpt-3.5-turbo': 'gpt-35-turbo',
+      'gpt-3.5-turbo-16k': 'gpt-35-turbo-16k',
+      'gpt-3.5-turbo-1106': 'gpt-35-turbo-1106',
       'gpt-4o': 'gpt-4o',
       'gpt-4o-mini': 'gpt-4o-mini',
       'gpt-4-turbo': 'gpt-4-turbo',
       'gpt-4': 'gpt-4',
-      'gpt-3.5-turbo-16k': 'gpt-35-turbo-16k',
-      'gpt-3.5-turbo-1106': 'gpt-35-turbo-1106',
       'o1-mini': 'o1-mini',
       'o1-preview': 'o1-preview',
       'claude-3-5-sonnet-20241022': 'claude-3-5-sonnet',
@@ -100,13 +102,11 @@ export const getChatCompletionStream = async (
 
     const model = modelmapping[config.model] || config.model;
 
+    // set api version to 2023-07-01-preview for gpt-4 and gpt-4-32k, otherwise use 2023-03-15-preview
     const apiVersion =
-      ['gpt-4', 'gpt-4o', 'gpt-4o-mini', 'gpt-4-turbo'].includes(model)
+      model === 'gpt-4' || model === 'gpt-4-32k'
         ? '2023-07-01-preview'
-        : ['gpt-3.5-turbo-16k', 'gpt-3.5-turbo-1106'].includes(model)
-        ? '2023-03-15-preview'
-        : '2023-01-01';
-
+        : '2023-03-15-preview';
     const path = `openai/deployments/${model}/chat/completions?api-version=${apiVersion}`;
 
     if (!endpoint.endsWith(path)) {
